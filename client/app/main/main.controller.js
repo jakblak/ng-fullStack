939d8275c04b283338d2ff1ac3393551a8a549e6
Patrick Schroeder <schroe9@gmail.com>
2015-06-09 23:17:58 -0700

    crud updates

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,43 +1,45 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-      .module('app')
-      .controller('MainCtrl', MainCtrl);
+  angular
+    .module('app')
+    .controller('MainCtrl', MainCtrl);
 
-    MainCtrl.$inject = ['$scope', '$http', '$location'];
+  MainCtrl.$inject = ['$scope', '$http', '$location'];
 
-    function MainCtrl($scope, $http, $location) {
+  function MainCtrl($scope, $http, $location) {
 
-      // GET all the things and show them
-      $http.get('/api/things')
+    // GET all the things and show them
+    $http.get('/api/things', {
+      cache: true
+    })
+      .success(function(data) {
+        $scope.things = data;
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+
+    $scope.create = function() {
+      $http.post('/api/things', {
+        name: $scope.name,
+        info: $scope.info
+      })
         .success(function(data) {
-          $scope.things = data;
+          $scope.things.push(data);
+          $scope.name = '';
+          $scope.info = '';
+          console.log(data);
+          //$location.url('/#things');
         })
         .error(function(data) {
           console.log('Error: ' + data);
         });
-
-      $scope.create = function() {
-        $http.post('/api/things', {
-          name: $scope.name,
-          info: $scope.info
-        })
-          .success(function(data) {
-            $scope.things.push(data);
-            $scope.name = '';
-            $scope.info = '';
-            //$location.url('/');
-            console.log(data);
-          })
-          .error(function(data) {
-            console.log('Error: ' + data);
-          });
-      };
-
-      $scope.deleteThing = function(thing) {
-        $http.delete('/api/things/' + thing._id);
-      };
     };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+  };
 
 })();
